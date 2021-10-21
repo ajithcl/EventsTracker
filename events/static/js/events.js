bce261ef3b3eb48feee766447423b57721273a57
ajithcl <clajith@gmail.com>
2021-10-21 18:52:26 +0530

    Updates.

--- events/static/js/events.js
+++ events/static/js/events.js
@@ -6,6 +6,9 @@
 
  $(document).ready(function () {
 
+    // Load event list
+    loadEvents();
+
     // Button click event for Create new entity
     $(document).on ("click", "#btn_create_event", function(e){
         e.preventDefault();
@@ -46,3 +49,41 @@
         create_event_btn.removeAttribute('hidden');
     })
  });
+
+ // function for loading events list
+ function loadEvents(){
+    table_body = document.getElementById('table_body');
+    $.ajax({
+        url : 'get_events',
+        type : 'GET',
+        datatype : 'JSON',
+        success : function (output){
+            if (output.result == 'success'){
+                event_list = output.data
+                event_list = JSON.parse(event_list)
+                event_table_html='';
+                if (event_list.length > 0){
+                    event_list.forEach(function(event_row){
+                        event_field = event_row['fields']
+                        console.log(event_field)
+                        event_table_html+=`
+                                <tr>
+                                  <td>${event_field["EventDate"]}</td>
+                                  <td>${event_field["EventName"]}</td>
+                                  <td>${event_field["Comments"]}</td>
+                                </tr>
+                        `;
+                    })
+                    table_body.innerHTML=event_table_html;
+                }
+            }
+            else{
+                console.log('error');
+            }
+
+        },
+        error : function (xhr, desc, err){
+        }
+
+    })
+ }
--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.display_events),
-    path('create_event', views.create_event)
+    path('create_event', views.create_event),
+    path('get_events', views.get_event_details)
 ]
